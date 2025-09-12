082f6f4b067434c329cfd3d4ce285ac843165dde
SridharA-start <sridhar7h@gmail.com>
2025-09-12 14:41:27 -0400

    initial commit

---
+++ src/main/java/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class HelloWorld {
+	public void weclomeMsg() {
+		System.out.println("welcome to java programming!");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld obj = new HelloWorld();
+		obj.weclomeMsg();
+
+	}
+
+}
