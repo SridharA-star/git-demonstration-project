cdcc5c05e8bad75ff8bd9f0699dc4597e6d1fbc9
SridharA-start <sridhar7h@gmail.com>
2025-09-16 14:40:27 -0400

    added c1 branch code

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public void weclomeMsg() { // from d1 branch
 		System.out.println("welcome to java programming!");
 	}
-    public void displayMsg() { // from branch c1
+    public void displayMsg() { // from c1 branch 
 		System.out.println("This is my first java program");
 	}
     // from branch b1
